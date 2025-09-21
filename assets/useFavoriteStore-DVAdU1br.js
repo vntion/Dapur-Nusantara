b001cb1ce4c465d304034ae9ce8222e670987bfc
Baskoro <139613178+vntion@users.noreply.github.com>
2025-09-21 21:31:34 +0700

    Updates

---
+++ assets/useFavoriteStore-DVAdU1br.js
@@ -0,0 +1,11 @@
+import{c as t,x as s,y as c}from"./index-DNGinK8m.js";/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=[["path",{d:"M12 6v6l4 2",key:"mmk7yg"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]],y=t("clock",i);/**
+ * @license lucide-react v0.544.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["path",{d:"M16 3.128a4 4 0 0 1 0 7.744",key:"16gr8j"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}]],k=t("users",d),l=s(e=>({favorites:[],addFavorite:a=>e(o=>({favorites:[...o.favorites,a]})),deleteFavoriteById:a=>e(o=>({favorites:o.favorites?.filter(r=>r.idMeal!==a)})),clearFavorites:()=>{e(()=>({favorites:[]})),c.success("Semua berhasil dihapus",{position:"bottom-right"})}}));export{y as C,k as U,l as u};
